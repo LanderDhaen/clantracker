a7aab1472e19a31ed1273bbb8708ffb15755312f
LanderDhaen <lander.dhaen@student.hogent.be>
2024-07-30 14:24:30 +0200

    chore(setup): Added ability to start backend with connection to DB

---
+++ backend/src/createServer.js
@@ -0,0 +1,53 @@
+const config = require("config");
+const Koa = require("koa");
+const { initializeLogger, getLogger } = require("./middleware/logging");
+const installRoutes = require("./routes");
+const { initializeData, shutdownData } = require("./data");
+const installMiddleware = require("./middleware/installMiddleware");
+
+const NODE_ENV = config.get("env");
+const LOG_LEVEL = config.get("log.level");
+const LOG_DISABLED = config.get("log.disabled");
+
+const dbclient = config.get("database.client");
+console.log("db client " + dbclient);
+
+module.exports = async function createServer() {
+  initializeLogger({
+    level: LOG_LEVEL,
+    disabled: LOG_DISABLED,
+    defaultMeta: {
+      NODE_ENV,
+    },
+  });
+
+  await initializeData();
+
+  const app = new Koa();
+
+  installMiddleware(app);
+
+  installRoutes(app);
+
+  return {
+    getApp() {
+      return app;
+    },
+
+    start() {
+      return new Promise((resolve) => {
+        const port = config.get("port");
+        app.listen(port, () => {
+          getLogger().info(`🚀 Server listening on http://localhost:${port}`);
+          resolve();
+        });
+      });
+    },
+
+    async stop() {
+      app.removeAllListeners();
+      await shutdownData();
+      getLogger().info("Goodbye! 👋");
+    },
+  };
+};
