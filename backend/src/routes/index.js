c281146a34cdd1086610d1bdf272f2d70ea9183c
LanderDhaen <lander.dhaen@student.hogent.be>
2024-07-30 15:52:36 +0200

    feat(member): Added ability to fetch all accounts

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,6 @@
 const Router = require("@koa/router");
 const installHealthRouter = require("./health");
+const installAccountRouter = require("./account");
 
 module.exports = (app) => {
   const router = new Router({
@@ -7,6 +8,7 @@ module.exports = (app) => {
   });
 
   installHealthRouter(router);
+  installAccountRouter(router);
 
   app.use(router.routes()).use(router.allowedMethods());
 };
---
+++ backend/src/services/account.js
@@ -0,0 +1,33 @@
+const { tables, getKnex } = require("../data/index");
+
+const getAllAccounts = async () => {
+  const accounts = await getKnex()(tables.account)
+    .join(tables.clan, `${tables.account}.clanID`, `${tables.clan}.ID`)
+    .join(
+      tables.townhall,
+      `${tables.account}.townhallID`,
+      `${tables.townhall}.ID`
+    )
+    .leftJoin(
+      { main: tables.account },
+      `${tables.account}.accountID`,
+      `main.ID`
+    )
+    .select([
+      `${tables.account}.ID`,
+      `${tables.account}.username as username`,
+      `${tables.account}.name as name`,
+      `${tables.account}.role as role`,
+      `${tables.account}.joined as joined`,
+      `${tables.account}.left as left`,
+      `${tables.clan}.name as clan`,
+      `${tables.townhall}.level as townhall`,
+      "main.name as main",
+    ]);
+
+  return accounts;
+};
+
+module.exports = {
+  getAllAccounts,
+};
