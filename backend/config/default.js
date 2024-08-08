929afc2382b6b0ad88151c3e0134e4e7b0d770a2
LanderDhaen <lander.dhaen@student.hogent.be>
2024-08-09 00:32:47 +0200

    chore(release-v1.1): Removed unnecessary config

--- backend/config/default.js
+++ backend/config/default.js
@@ -11,27 +11,4 @@ module.exports = {
   database: {
     client: "pg",
   },
-  auth: {
-    argon: {
-      saltLength: 16,
-      hashLength: 32,
-      timeCost: 6,
-      memoryCost: 2 ** 17, // 128 MB
-    },
-    jwt: {
-      secret:
-        "eenveeltemoeilijksecretdatniemandooitzalradenandersisdesitegehacked",
-      expirationInterval: 60 * 60 * 1000, // ms (1 hour)
-      refreshInterval: 5 * 1000, // ms (7 days)
-      issuer: "clantracker.be",
-      audience: "clantracker.be",
-    },
-    session: {
-      key: "clantracker.sid",
-      maxAge: 60 * 60 * 24 * 7 * 1000, // ms (1 week)
-      overwrite: true,
-      httpOnly: true,
-      signed: true,
-    },
-  },
 };
